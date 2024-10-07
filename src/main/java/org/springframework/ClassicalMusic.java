9f1797c71ae6cc64501bca744993c7aff6ef6f93
Alex Nikonov <60483531+lexxA87@users.noreply.github.com>
2024-10-07 19:44:03 +0400

    Added test for Factory-method

--- src/main/java/org/springframework/ClassicalMusic.java
+++ src/main/java/org/springframework/ClassicalMusic.java
@@ -1,8 +1,15 @@
 package org.springframework;
 
 public class ClassicalMusic implements Music{
+    private ClassicalMusic() {}
+
     @Override
     public String getSong() {
         return "9th sonata";
+    }
+
+    public static ClassicalMusic getClassicalMusic() {
+        System.out.println("Factory for Classical Music");
+        return new ClassicalMusic();
     }
 }
